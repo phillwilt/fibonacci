9421a1d8339730616433e755e03b1e617d49af4f
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-17 14:59:57 -0700

    remove superflous tests

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -11,16 +11,6 @@ describe Fibonacci do
     fibonacci.nth_number(1).must_be_kind_of Numeric
   end
 
-  it 'returns 1 for the first Fibonacci number' do
-    fibonacci = Fibonacci.new
-    assert_equal 1, fibonacci.nth_number(1)
-  end
-
-  it 'returns 1 for the second Fibonacci number' do
-    fibonacci = Fibonacci.new
-    assert_equal 1, fibonacci.nth_number(1)
-  end
-
   it 'returns correct Fibonacci numbers' do
     fibonacci_numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233,
                          377, 610]
