f23ecddee70848fb9ee1d9531d29b8f984191484
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-17 15:27:16 -0700

    refactor fibonacci for style

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -3,30 +3,24 @@
 class Fibonacci
   def nth_number(fibonacci_index)
     return 1 unless fibonacci_index > 2
-    nth_number_helper(1, 1, 3, fibonacci_index)
+    nth_number_helper(fibonacci_index)
   end
 
   private
 
-  def nth_number_helper(
-                        fib_num_back_one,
-                        fib_num_back_two,
-                        current_index,
-                        fibonacci_index
-                        )
-    current_fib_num = fib_num_back_two + fib_num_back_one
+  def nth_number_helper(fibonacci_index, options = nil)
+    options ||= { fib_num_back_one: 1, fib_num_back_two: 1, current_index: 3 }
 
-    if current_index == fibonacci_index
+    current_fib_num = options.fetch(:fib_num_back_two) +
+                      options.fetch(:fib_num_back_one)
+
+    if options.fetch(:current_index) == fibonacci_index
       current_fib_num
     else
-      current_index += 1
-
-      nth_number_helper(
-                        current_fib_num,
-                        fib_num_back_one,
-                        current_index,
-                        fibonacci_index
-                        )
+      options[:current_index] += 1
+      options[:fib_num_back_two] = options.fetch(:fib_num_back_one)
+      options[:fib_num_back_one] = current_fib_num
+      nth_number_helper(fibonacci_index, options)
     end
   end
 end
