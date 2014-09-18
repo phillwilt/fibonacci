688a7d0847c496f801aabd3d1907b95e69b2a264
Phillip Wilt <phillip.wilt@gmail.com>
2014-09-18 10:43:40 -0700

    removed magic numbers

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,15 +1,22 @@
 # fibonnaci.rb
 
 class Fibonacci
+  MIN_FIB_INDEX = 2
+  FIRST_FIB_NUM = 1
+  SECOND_FIB_NUM = 1
+
   def nth_number(fibonacci_index)
-    return 1 unless fibonacci_index > 2
+    return 1 unless fibonacci_index > MIN_FIB_INDEX
     nth_number_helper(fibonacci_index)
   end
 
   private
 
   def nth_number_helper(fibonacci_index, options = nil)
-    options ||= { fib_num_back_one: 1, fib_num_back_two: 1, current_index: 3 }
+    options ||= {
+                  fib_num_back_one: SECOND_FIB_NUM,
+                  fib_num_back_two: FIRST_FIB_NUM,
+                  current_index: 3 }
 
     current_fib_num = options.fetch(:fib_num_back_two) +
                       options.fetch(:fib_num_back_one)
